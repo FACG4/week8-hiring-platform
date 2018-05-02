36be789fc23af5d2f760585e8f81af77bf695b7c
Farah Zaqout <farah.zaqout@gmail.com>
2018-05-02 19:26:31 +0300

    created the db-build function

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -2,11 +2,7 @@ const dbConnection = require('./db_connection');
 const fs = require('fs');
 const path = require('path');
 
-
-const sql = fs.readFile(path.join(__dirname, 'db_build.sql'), (err, file) => {
-  if (err) return (err);
-  return file.toString();
-});
+const sql = fs.readFileSync(path.join(__dirname, 'db_build.sql')).toString();
 
 const runBuild = (cb) => {
   dbConnection.query(sql, (err, res) => {
@@ -14,5 +10,10 @@ const runBuild = (cb) => {
     cb(null, res);
   });
 };
+
+// console.log('k');
+// runBuild((err,res)=>{
+//   console.log('i');
+// })
 
 module.exports = runBuild;
