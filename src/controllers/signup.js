4378404ce5154068b5dfb2caf0a1692e437b25f8
sallamtanna <sallamtanna2015@hotmail.com>
2018-05-03 13:36:45 +0300

    trying to insert into employees table

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -8,12 +8,32 @@ exports.get = (req, res) => {
 
 exports.post = (req, res) => {
   const userData=req.body;
-  // console.log(userData);
-  signup.register(userData,(err,result)=>{
+  const isEmployee = req.body.isEmployee;
+  // console.log(isEmployee);
+
+if(isEmployee === undefined){
+   signup.addEmployers(userData,(err,result)=>{
     if (err) {
       return res.status(409).send();
     }
     return res.send();
   })
   res.render('login')
+}
+
+
+else{
+ 
+  console.log(req.body.skill);
+   signup.addEmployee(userData,(err,result)=>{
+    if (err) {
+      return res.status(409).send();
+    }
+    return res.send();
+  })
+  res.render('login')
+}
+
+
+
 };
--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -8,14 +8,13 @@
   );
 CREATE TABLE employees(
   id SERIAL PRIMARY KEY,
-  username VARCHAR(30) NOT NULL,
-  fname VARCHAR(30) NOT NULL,
+   fname VARCHAR(30) NOT NULL,
   Lname VARCHAR(30) NOT NULL,
   email VARCHAR(30) NOT NULL,
   password VARCHAR(80) NOT NULL,
-  skill_id INTEGER REFERENCES skills(id),
-  carrer VARCHAR(30) NOT NULL,
-  mobile VARCHAR(30) Not Null
+  skill_id INTEGER REFERENCES skills(id)
+  -- carrer VARCHAR(30) NOT NULL,
+  -- mobile VARCHAR(30) Not Null
 );
 
 CREATE TABLE employers(
@@ -26,13 +25,13 @@ CREATE TABLE employers(
   password VARCHAR(80) NOT NULL
 );
 
-INSERT INTO skills(title) VALUES ('Design'), ('Programming'),('Testing'),('mentoring'),('Content writing'),
-('scrum master'),('coordinator');
+INSERT INTO skills(title) VALUES ('Design'), ('Programming'),('Testing'),('Mentoring'),('Content writing'),
+('Scrum master'),('Coordinator');
 
 
-INSERT INTO employees(username, fname, lname, email, password, skill_id, carrer) VALUES
-('Sallam1994' , 'sallam' , 'Tanna', 'sallamtanna2015@hotmail.com' , '123456789', 1, 'Engineer'),
-('Haneen Shahwan' , 'Haneen' , 'Shahwan', 'Haneen2015@hotmail.com' , '123456789', 2, 'Engineer');
+INSERT INTO employees( fname, lname, email, password, skill_id) VALUES
+( 'sallam' , 'Tanna', 'sallamtanna2015@hotmail.com' , '123456789', 1),
+( 'Haneen' , 'Shahwan', 'Haneen2015@hotmail.com' , '123456789', 2);
 
 
 
